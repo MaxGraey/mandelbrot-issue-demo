61a2d8d684d8190091de78dafb9310d14df67a0f
MaxGraey <maxgraey@gmail.com>
2020-03-02 01:36:25 +0200

    add precalculated values

--- tests/failed.cpp
+++ tests/failed.cpp
@@ -24,31 +24,51 @@ int64_t ulpDiffs(double x, double y) {
 
 int main() {
   const double inputs[] = {
-    9.39272367658092655e+00,
-    7.14726094603619089e+00,
-    8.60221196398579480e+00,
-    4.13931110013671955e+00,
-    7.43155422527207676e+00,
-    8.17974828584226366e+00,
-    4.94454417465960727e+00,
-    6.70535708800538277e+00,
-    7.98827495796161813e+00,
-    5.95566127061565265e+00,
-    1.07102800210768265e+01,
-    8.57346179090154159e+00
+     9.39272367658092655e+00,
+     7.14726094603619089e+00,
+     8.60221196398579480e+00,
+     4.13931110013671955e+00,
+     7.43155422527207676e+00,
+     8.17974828584226366e+00,
+     4.94454417465960727e+00,
+     6.70535708800538277e+00,
+     7.98827495796161813e+00,
+     5.95566127061565265e+00,
+     1.07102800210768265e+01,
+     8.57346179090154159e+00
+  };
+
+  // precalculated values
+  const double origs[] = {
+     1.63457069072895561e-01,
+    -2.42016750151798320e-02,
+     1.05691066987024976e-01,
+    -4.93571336774668801e-01,
+     4.13102187661396926e-03,
+     7.15302395717923589e-02,
+    -3.23475509963837504e-01,
+    -7.17951175532505204e-02,
+     5.51781837370421552e-02,
+    -1.64607645731548374e-01,
+     2.45619804628072896e-01,
+     1.03444996051807361e-01
   };
 
   printf("log2(0.5 * log(x)):\n");
 
   for (int i = 0; i < sizeof inputs / 8; i++) {
-    double input    = inputs[i];
-    double actual   = _log2(0.5 * _log(input)); // musl 1.2.0
-    double expected =  log2(0.5 *  log(input)); // libc
+    double input = inputs[i];
+    double orig  = origs[i];
+    double musl  = _log2(0.5 * _log(input)); // musl 1.2.0
+    double libc  =  log2(0.5 *  log(input)); // libc
     printf("\narg:    %.17g\n", input);
     printf("---------------------------------------------------\n");
-    printf("actual:% .17f (% .13a)\n", actual,   actual);
-    printf("expect:% .17f (% .13a)\n", expected, expected);
-    printf("ULP diff: %llu\n", ulpDiffs(actual, expected));
+    printf("actual musl:% .17f (% .13a)\n", musl, musl);
+    printf("actual libc:% .17f (% .13a)\n", libc, libc);
+    printf("expected:   % .17f (% .13a)\n", orig, orig);
+    printf("ULP diff (musl, libc): %llu\n", ulpDiffs(musl, libc));
+    printf("ULP diff (musl, orig): %llu\n",  ulpDiffs(musl, orig));
   }
+
   return 0;
 }
